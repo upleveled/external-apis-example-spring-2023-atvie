48b88c56077fa8ed51d187a46cf087aaf3f45a52
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-06-30 11:18:46 +0200

    Rename route to ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,9 @@
 import { NextResponse } from 'next/server';
 
+type StripeResponse = {
+  sessions: string;
+};
+
 export function GET() {
   return NextResponse.json({
     sessions: '/api/sessions',
